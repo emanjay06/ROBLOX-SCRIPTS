9ccf7ff48e06319e30351590f4b1d88a0d94a17a
emanjay06 <108910739+emanjay06@users.noreply.github.com>
2022-07-09 21:12:27 +0100

    Create Millionaire Tycoon 2 Script.lua

---
+++ Millionaire Tycoon 2 Script.lua
@@ -0,0 +1,11 @@
+-- Game Name : Millionaire Empire Tycoon
+-- Game Link : https://www.roblox.com/games/6677985923/X2-CASH-Millionaire-Empire-Tycoon
+--[[Script Updates :
+   [+] Fixed
+]]
+-- Script :
+_G.RedGUI = false
+_G.Theme = "Dark" -- Must disable or remove _G.RedGUI to use
+--Themes: Light, Dark, Mocha, Aqua and Jester
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MillionaireEmpireTycoon.lua"))()
